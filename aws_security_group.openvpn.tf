653b9f5d7a3ee36bc098ce8ff8c26cdf6293b4cf
Janis Peisenieks <janis@peisenieks.lv>
2018-12-21 09:07:01 +0200

    Refactor security group to allow conditional attaching of rules

--- aws_security_group.openvpn.tf
+++ aws_security_group.openvpn.tf
@@ -4,51 +4,63 @@ resource "aws_security_group" "openvpn" {
   description = "${upper(terraform.workspace)} Public VPN Security Rules"
   vpc_id      = "${var.network_vpc_id}"
 
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["${var.openvpn_all_access}"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["${var.openvpn_ssh_access}"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["${var.openvpn_https_access}"]
-  }
-
-  ingress {
-    from_port   = 943
-    to_port     = 943
-    protocol    = "tcp"
-    cidr_blocks = ["${var.openvpn_admin_access}"]
-  }
-
-  ingress {
-    from_port   = 1194
-    to_port     = 1194
-    protocol    = "udp"
-    cidr_blocks = ["${var.openvpn_udp_access}"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   tags {
     Name        = "${lower(terraform.workspace)}_${lower(var.stack_name)}"
     Environment = "${lower(terraform.workspace)}"
     Stack       = "${lower(var.stack_name)}"
   }
+}
+
+resource "aws_security_group_rule" "openvpn_all_access" {
+  type              = "ingress"
+  to_port           = 0
+  from_port         = 0
+  protocol          = "-1"
+  security_group_id = "${aws_security_group.openvpn.id}"
+  cidr_blocks       = ["${var.openvpn_all_access}"]
+}
+
+resource "aws_security_group_rule" "openvpn_ssh_access" {
+  type              = "ingress"
+  to_port           = 22
+  from_port         = 22
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.openvpn.id}"
+  cidr_blocks       = ["${var.openvpn_ssh_access}"]
+}
+
+resource "aws_security_group_rule" "openvpn_https_access" {
+  type              = "ingress"
+  to_port           = 443
+  from_port         = 443
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.openvpn.id}"
+  cidr_blocks       = ["${var.openvpn_https_access}"]
+}
+
+resource "aws_security_group_rule" "openvpn_admin_access" {
+  type              = "ingress"
+  to_port           = 943
+  from_port         = 943
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.openvpn.id}"
+  cidr_blocks       = ["${var.openvpn_admin_access}"]
+}
+
+resource "aws_security_group_rule" "openvpn_udp_access" {
+  type              = "ingress"
+  to_port           = 1194
+  from_port         = 1194
+  protocol          = "udp"
+  security_group_id = "${aws_security_group.openvpn.id}"
+  cidr_blocks       = ["${var.openvpn_udp_access}"]
+}
+
+resource "aws_security_group_rule" "openvpn_egress" {
+  type              = "egress"
+  to_port           = 0
+  from_port         = 0
+  protocol          = "-1"
+  security_group_id = "${aws_security_group.openvpn.id}"
+  cidr_blocks       = ["0.0.0.0/0"]
 }
